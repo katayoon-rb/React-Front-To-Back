66b7a439596bca9a2dcd764391cd83ccabf647b8
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-02-10 12:59:39 +0330

    Add files via upload

---
+++ 2 Github Finder App/src/components/layout/Spinner.jsx
@@ -0,0 +1,14 @@
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <div className='mt-20'>
+      <img src={spinner} width={180}
+        className='text-center mx-auto'
+        alt='Loading...'
+      />
+    </div>
+  )
+}
+
+export default Spinner
---
+++ 2 Github Finder App/src/context/github/GithubActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const github = axios.create({
+  baseURL: 'https://api.github.com'
+})
+
+
+// Search results
+export const searchUsers = async text => {
+  const params = new URLSearchParams({ q: text })
+  const response = await github.get(`/search/users?${params}`)
+  return response.data.items
+}
+
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`)
+  ])
+  return { user: user.data, repos: repos.data }
+}
